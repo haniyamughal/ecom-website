ca1ac8e9b37a09842618d366d1aa8c8db3dfd034
haniyamughal <160358992+haniyamughal@users.noreply.github.com>
2024-12-10 02:34:15 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,12 @@ import DiscountItem from "../components/discountItem";
 import FeaturedProducts from "../components/featured product";
 import Footer from "../components/footer";
 import Header from "../components/header";
-import HeroSection from "@/components/hero";
-import LatestProducts from "@/components/latestproduct";
-import Navbar from "@/components/navbar";
-import Shopex from "@/components/shopex";
-import TopCategory from "@/components/topcategory";
-import TrendingProducts from "@/components/trending products";
-import Image from "next/image";
+import HeroSection from "../components/hero";
+import LatestProducts from "../components/latestproduct";
+import Navbar from "../components/navbar";
+import Shopex from "../components/shopex";
+import TopCategory from "../components/topcategory";
+import TrendingProducts from "../components/trending products";
 
 export default function Page() {
   return (
