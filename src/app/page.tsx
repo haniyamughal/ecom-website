6a535799ed3cebe3be1c4416f8e5bf9d7dcd2c37
haniyamughal <160358992+haniyamughal@users.noreply.github.com>
2024-12-10 02:27:59 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Banner2 from "@/components/banner2";
-import Blogspost from "@/components/blogpost";
-import DiscountItem from "@/components/discountItem";
-import FeaturedProducts from "@/components/featured product";
-import Footer from "@/components/footer";
-import Header from "@/components/header";
+import Banner2 from "../components/banner2";
+import Blogspost from "../components/blogpost";
+import DiscountItem from "../components/discountItem";
+import FeaturedProducts from "../components/featured product";
+import Footer from "../components/footer";
+import Header from "../components/header";
 import HeroSection from "@/components/hero";
 import LatestProducts from "@/components/latestproduct";
 import Navbar from "@/components/navbar";
@@ -12,7 +12,7 @@ import TopCategory from "@/components/topcategory";
 import TrendingProducts from "@/components/trending products";
 import Image from "next/image";
 
-export default function Home() {
+export default function Page() {
   return (
  <div>
   <Header />
